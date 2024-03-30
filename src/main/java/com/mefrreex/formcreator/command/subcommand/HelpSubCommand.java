1c1161c1061257588680ced879d8894611c3565a
MEFRREEX <mefrreex@gmail.com>
2024-03-30 10:04:15 +0200

    feat: reload command

--- src/main/java/com/mefrreex/formcreator/command/subcommand/HelpSubCommand.java
+++ src/main/java/com/mefrreex/formcreator/command/subcommand/HelpSubCommand.java
@@ -9,12 +9,18 @@ public class HelpSubCommand extends BaseSubCommand {
 
     public HelpSubCommand(BaseCommand command) {
         super("help", "Help", command);
+        this.setPermission("formcreator.help");
     }
 
     @Override
     public boolean execute(CommandSender sender, String commandLabel, String label, String[] args) {
+        if (!this.testPermission(sender)) {
+            return false;
+        }
+
         StringBuilder builder = new StringBuilder();
         builder.append(Language.get("command-help-available"));
+
         for (BaseSubCommand sub : this.getCommand().getSubcommands().values()) {
             if (sub.testPermission(sender)) {
                 String name = sub.getName();
@@ -22,6 +28,7 @@ public class HelpSubCommand extends BaseSubCommand {
                 builder.append("\n" + Language.get("command-help-format", commandLabel, name, description));
             }
         }
+
         sender.sendMessage(builder.toString());
         return true;
     }
